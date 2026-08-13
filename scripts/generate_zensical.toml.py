81b3341162444a326265907fb207bbefe6d11e81
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2026-08-13 18:46:42 +0330

    use github clone action

--- scripts/generate_zensical.toml.py
+++ scripts/generate_zensical.toml.py
@@ -16,36 +16,10 @@ def load_operators():
         return tomllib.load(f)["operators"]
 
 
-def clone_operator(operator) -> str:
-    """Cline the operator and return the path to its repo"""
-    target = REPOS / operator["name"]
-
-    if target.exists():
-        shutil.rmtree(target)
-
-    r = subprocess.run(
-        [
-            "git",
-            "clone",
-            f"https://github.com/mahdihaghverdi/{operator['name']}.git",
-            str(target)
-        ],
-        check=True,
-        capture_output=True,
-    )
-    print(r)
-
-    return target
-
-
 def main():
     operators = load_operators()
 
     REPOS.mkdir(exist_ok=True)
-
-    for operator in operators:
-        repo = clone_operator(operator)
-
     print(os.listdir(REPOS))
 
 
