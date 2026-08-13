b8c9f81f738f663d820b2d53c0822ff0037ea5be
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2026-08-13 19:28:42 +0330

    print(toml_acceptable_nav)

--- scripts/generate_zensical.toml.py
+++ scripts/generate_zensical.toml.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import shutil
@@ -19,13 +20,36 @@ def copy_docs(operator_name: str):
     source = ROOT / operator_name / "docs"
     target = DOCS / operator_name
     shutil.copytree(source, target)
-    print(os.listdir(target))
+
+
+def create_the_canonical_nav(operators: list[dict]) -> list[dict]:
+    # read the k8s-operators-docs zensical.nav.txt
+    ZENSICAL_NAV_CONF = "zensical.nav.txt"
+    nav = []
+    with open(ROOT / ZENSICAL_NAV_CONF) as f:
+        nav.append(eval(f.read()))
+
+    for operator in operators:
+        operator_name = operator["name"]
+        with open(ROOT / operator_name / ZENSICAL_NAV_CONF) as f:
+            nav.append(eval(f.read()))
+    return nav
+
+
+def make_nav_toml_acceptable(nav: list[dict]) -> str:
+    toml_acceptable_nav = json.dumps(str(nav), indent=4).replace(":", ' = ')
+    return toml_acceptable_nav
 
 
 def main():
     operators = load_operators()
     for operator in operators:
         copy_docs(operator['name'])
+
+    canonical_nav = create_the_canonical_nav(operators)
+    toml_acceptable_nav = make_nav_toml_acceptable(canonical_nav)
+    print(toml_acceptable_nav)
+
 
 if __name__ == "__main__":
     main()
