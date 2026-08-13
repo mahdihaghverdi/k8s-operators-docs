1fba0ebbcecb036a685a2866d7af867dd724ce74
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2026-08-13 18:38:34 +0330

    test clone function

--- scripts/generate_zensical.toml.py
+++ scripts/generate_zensical.toml.py
@@ -1,5 +1,8 @@
 import logging
 import os
+import shutil
+import subprocess
+import tomllib
 from pathlib import Path
 
 logger = logging.getLogger("generator")
@@ -8,5 +11,43 @@ ROOT = Path(__file__).parent.parent
 DOCS = ROOT / "docs"
 REPOS = ROOT / ".repos"
 
-for thing in (ROOT, DOCS, REPOS):
-    print(os.listdir(thing))
+def load_operators():
+    with open(ROOT / "operators.config.toml", "rb") as f:
+        return tomllib.load(f)["operators"]
+
+
+def clone_operator(operator) -> str:
+    """Cline the operator and return the path to its repo"""
+    target = REPOS / operator["name"]
+
+    if target.exists():
+        shutil.rmtree(target)
+
+    subprocess.run(
+        [
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            f"https://github.com/mahdihaghverdi/{operator['name']}.git",
+            str(target)
+        ],
+        check=True,
+    )
+
+    return target
+
+
+def main():
+    operators = load_operators()
+
+    REPOS.mkdir(exist_ok=True)
+
+    for operator in operators:
+        repo = clone_operator(operator)
+
+    print(os.listdir(REPOS))
+
+
+if __name__ == "__main__":
+    main()
