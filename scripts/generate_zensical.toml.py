518d9fa73661bb07a47f81901b0841293072c1c6
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2026-08-13 19:14:33 +0330

    copy docs to docs

--- scripts/generate_zensical.toml.py
+++ scripts/generate_zensical.toml.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import shutil
-import subprocess
 import tomllib
 from pathlib import Path
 
@@ -9,18 +8,25 @@ logger = logging.getLogger("generator")
 
 ROOT = Path(__file__).parent.parent
 DOCS = ROOT / "docs"
-REPOS = ROOT / ".repos"
+
 
 def load_operators():
     with open(ROOT / "operators.config.toml", "rb") as f:
         return tomllib.load(f)["operators"]
 
 
+def copy_docs(operator_name: str):
+    source = ROOT / operator_name / "docs"
+    target = DOCS / operator_name
+    shutil.copytree(source, target)
+
+
 def main():
     operators = load_operators()
-    REPOS.mkdir(exist_ok=True)
-    print(os.listdir(ROOT))
+    for operator in operators:
+        copy_docs(operator)
 
+    print(os.listdir(DOCS))
 
 if __name__ == "__main__":
     main()
